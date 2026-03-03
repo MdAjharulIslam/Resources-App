a8b84a8f9787bae1bd907672dde1b6ee9f091d4b
ajharul <mdajharulislam327@gmail.com>
2026-03-04 02:00:52 +0600

    add a nevigation on home page to goto resources page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-
+Route::get('/', function(){
+    return "<a href='/resources'>goto Resources</a>";
+});
 
 Route::resource('resources', UserController::class);
